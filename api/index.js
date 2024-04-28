747561a603e546dedaa523d3168be4da46432d12
dimesha <dimesha99kanchani@gmail.com>
2024-04-29 00:45:50 +0530

    setup get and post method rotes

--- api/index.js
+++ api/index.js
@@ -14,8 +14,34 @@ mongoose.connect('mongodb://localhost:27017/notes', {useNewUrlParser:true, useUn
 
 
 //create route
-app.get('/api/notes', (req, res) => {
-    res.status(200).json({message: "This is my api.."})
+app.get('/api/notes', async(req, res) => {
+    try{
+        const notes = await Note.find();
+        res.status(200).json({message:"Fetched Note Successfully..", data: notes});
+    }catch(err){
+        res.status(500).json({message:"Error fetching notes..", error: err});
+    }
+    
+})
+
+
+app.post('/api/notes', async(req, res) => {
+    try{
+        const newNote = new Note({
+            title: req.body.title,
+            content: req.body.content
+        })
+        const saveNote = await newNote.save();
+        res.status(201).json({message:"Note created successfully", data:saveNote});
+    }catch(err){
+        res.status(500).json({message:"Error fetching notes..", error: err});
+    }
+
+})
+
+
+app.all('*', (req, res) => {
+    res.status(404).json("<h1>404 | Page not found");
 })
 
 //creaate the server
